102fdee5e4cb69eefbf21d332d9f6323ee61b45b
sachi <sachi@Administrator>
2023-03-06 13:35:01 +0530

    First Commit

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -7,6 +7,7 @@ package dao;
 import javax.swing.JOptionPane;
 import model.User;
 import java.sql.*;
+import java.util.ArrayList;
 
 
 
@@ -65,5 +66,33 @@ public class UserDao {
     public static void update(String email,String newPassword){
         String query = "update user set password = '" + newPassword + "'where email= '" + email + "'";
         DbOperations.setDataOrDelete(query, "Password changed Successfully");
+    }
+    public static ArrayList<User> getAllrecords(String email){
+        ArrayList<User> arrayList = new ArrayList<>();
+        try{
+            ResultSet rs = DbOperations.getData("select *from user where email like '%"+email+"%'");
+            while(rs.next()){
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setMobileNumber(rs.getString("mobileNumber"));
+                user.setSecurityQuestion(rs.getString("securityQuestion"));
+                user.setStatus(rs.getString("status"));
+                arrayList.add(user);
+                
+                
+            }
+        
+        }
+        catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e);
+        }
+        return arrayList;
+    
+    }
+    public static void changeStatus(String email,String status){
+        String query = "update user set status='"+status+"'where email='"+email+"'";
+        DbOperations.setDataOrDelete(query, "Status Changed Successfully");
     }
 }
--- src/dao/tables.java
+++ src/dao/tables.java
@@ -16,9 +16,11 @@ public class tables {
             String userTable = "create table user(id int AUTO_INCREMENT primary key ,name varchar(200),email varchar(200),mobileNumber varchar(10),address varchar(200),password varchar(200),securityQuestion varchar(200),answer varchar(200),status varchar(20),UNIQUE(email))";
             String adminDetails = "insert into user(name,email,mobileNumber,address,password,securityQuestion,answer,status ) values('Admin','admin@gmail.com','1234567890','Colombo','admin','What is your name?','Kimbula','true')";
             String categoryTable = "create table category(id int AUTO_INCREMENT primary key,name varchar(200))";
+            String productTable = "create table product(id int AUTO_INCREMENT primary key,name varchar(200),category varchar(200), price varchar(200))";
             DbOperations.setDataOrDelete(userTable, "User Table Created Successfully");
             DbOperations.setDataOrDelete(adminDetails, " Admin Details added Successfully");
-             DbOperations.setDataOrDelete(categoryTable, " Category Table Created Successfully");
+            DbOperations.setDataOrDelete(categoryTable, " Category Table Created Successfully");
+            DbOperations.setDataOrDelete(productTable,"Product Table Created Successfully" );
             
             
         }
